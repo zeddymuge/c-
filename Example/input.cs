8790d38668095d0991626c1ac8506f66bbca07aa
zeddymuge <cheroozeddy@gmail.com>
2022-08-29 03:09:02 -0700

    input field

---
+++ Example/input.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter username:");
+			string userName = Console.ReadLine();
+			Console.WriteLine("Username is: " + userName);
+		}
+	}
+}
