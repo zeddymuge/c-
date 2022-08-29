d522f24d4365612ebf38b691aa494ff1fcab50e6
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-29 17:55:26 +0300

    task done

---
+++ OOP/calling_methods.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MyApplication
+{
+  class Car
+  {
+    string color;                 // field
+    int maxSpeed;                 // field
+    public void fullThrottle()    // method
+    {
+      Console.WriteLine("The car is going as fast as it can!");
+    }
+
+    static void Main(string[] args)
+    {
+      Car myObj = new Car();
+      myObj.fullThrottle();  // Call the method
+    }
+  }
+}
