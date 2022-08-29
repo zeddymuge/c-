701fb4a28c0c20a2ec4850a5d22c7bb8d4831a0b
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-29 16:58:31 +0300

    Create upper_lower.cs

---
+++ Example/upper_lower.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string txt = "Hello World";
+      Console.WriteLine(txt.ToUpper());   // Outputs "HELLO WORLD"
+      Console.WriteLine(txt.ToLower());   // Outputs "hello world"
+    }
+  }
+}
