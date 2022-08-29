ca248f9a44316465dca3e7abc67774982499d7b7
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-29 17:34:54 +0300

    Update method-test_1.cs

---
+++ methods/method-test_1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod()
+    {
+      Console.WriteLine("my first method!");
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod();
+    }
+  }
+}
