773fecefd413f9a6cf48e11f2924c1b467d998cb
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2022-08-31 15:17:35 +0300

    task done

---
+++ methods/property.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Animal dog = new Animal();
+      dog.Name = "simba";
+      Console.WriteLine(myObj.Name);
+    }
+  }
+}
